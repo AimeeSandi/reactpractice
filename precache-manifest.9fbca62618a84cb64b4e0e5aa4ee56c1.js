932b9f17cd8a5675074348673351b1383a5a972b
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-13 15:10:14 +0100

    Updates

---
+++ precache-manifest.9fbca62618a84cb64b4e0e5aa4ee56c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351fb14ef6463be27a7505e388a84d56",
+    "url": "/reactpractice/index.html"
+  },
+  {
+    "revision": "8d48604502d0ada018c5",
+    "url": "/reactpractice/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactpractice/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8d48604502d0ada018c5",
+    "url": "/reactpractice/static/js/main.d091f1a0.chunk.js"
+  },
+  {
+    "revision": "0bc16deefa99f8785e54",
+    "url": "/reactpractice/static/js/runtime~main.94f820ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactpractice/static/media/logo.5d5d9eef.svg"
+  }
+]);
